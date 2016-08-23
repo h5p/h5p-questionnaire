ea5b1db5de3ba910a1c35eca41b997065ad14a78
Thomas Marstrander <thomasmars87@gmail.com>
2016-08-23 12:39:25 +0200

    Build styles into separate css files, since inlining it had some complications when debugging other code.
    Added separate webpack config for dev and prod, reduces complexity for prod config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,19 @@
 var autoprefixer = require('autoprefixer');
 var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: {
-    '../dist/dist': "./src/entries/dist.js",
-    'dev': "./src/entries/dev.js"
-  },
+  entry: "./src/entries/dist.js",
   output: {
-    path: path.join(__dirname, '/build'),
-    filename: "[name].js"
+    path: path.join(__dirname, '/dist'),
+    filename: "dist.js"
   },
   module: {
     loaders: [
       {
         test: /\.js$/,
         include: [
-          path.resolve(__dirname, "src/content"),
           path.resolve(__dirname, "src/scripts"),
           path.resolve(__dirname, "src/entries")
         ],
@@ -25,39 +22,17 @@ module.exports = {
       {
         test: /\.css$/,
         include: [
-          path.resolve(__dirname, "src/content")
-        ],
-        loader: "style!css!postcss"
-      },
-      {
-        test: /\.css$/,
-        include: [
           path.resolve(__dirname, "src/scripts")
         ],
-        loader: "style!css!postcss?sourceMap=inline"
-      },
-      {
-        test: /\.json$/,
-        include: path.resolve(__dirname, "src/content"),
-        loader: 'json'
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "url?limit=10000&mimetype=application/font-woff"
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "file"
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader!postcss")
       }
     ]
   },
   postcss: function () {
     return [autoprefixer];
   },
-  devtool: 'inline-source-map',
-  devServer: {
-    port: 8050,
-    contentBase: "./build",
-    quiet: true
-  }
+  plugins: [
+    new ExtractTextPlugin("styles.css")
+  ],
+  devtool: 'inline-source-map'
 };
---
+++ webpack.dev.config.js
@@ -0,0 +1,54 @@
+var autoprefixer = require('autoprefixer');
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: "./src/entries/dev.js",
+  output: {
+    path: path.join(__dirname, '/build'),
+    filename: "dev.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, "src/content"),
+          path.resolve(__dirname, "src/scripts"),
+          path.resolve(__dirname, "src/entries")
+        ],
+        loader: 'babel'
+      },
+      {
+        test: /\.css$/,
+        include: [
+          path.resolve(__dirname, "src/content"),
+          path.resolve(__dirname, "src/scripts")
+        ],
+        loader: "style!css!postcss"
+      },
+      {
+        test: /\.json$/,
+        include: path.resolve(__dirname, "src/content"),
+        loader: 'json'
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file"
+      }
+    ]
+  },
+  postcss: function () {
+    return [autoprefixer];
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 8050,
+    contentBase: "./build",
+    quiet: true
+  }
+};
